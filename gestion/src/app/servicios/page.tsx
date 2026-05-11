b992dfbd970063dde57ffd20b526e260787e45a8
julian rodriguez <julianestebanrodriguezpulido@gmail.com>
2026-05-11 14:08:29 -0500

    implementacion de Suspense para servicio

--- gestion/src/app/servicios/page.tsx
+++ gestion/src/app/servicios/page.tsx
@@ -1,5 +1,10 @@
 import Soluciones from "@/UI/soluciones";
+import { Suspense } from "react";
 
-export default function page () {
-  return <Soluciones/>
+export default function page() {
+  return (
+    <Suspense fallback={<div>Cargando...</div>}>
+      <Soluciones />
+    </Suspense>
+  );
 }
